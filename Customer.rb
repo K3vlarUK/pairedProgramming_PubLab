57cabfdcdb4ee485b823c154677d693a7df6eb49
Kevin McDermott <kmcdermott31@gmail.com>
2019-09-04 11:32:47 +0100

    MVP completed

--- Customer.rb
+++ Customer.rb
@@ -7,4 +7,13 @@ class Customer
     @wallet = wallet
   end
 
+  def reduce_wallet(drink)
+    @wallet -= drink.get_price()
+  end
+
+  def buys_drink(drink,pub)
+    reduce_wallet(drink)
+    pub.increase_till(drink)
+  end
+
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -2,6 +2,7 @@ require("minitest/autorun")
 require("minitest/pride")
 require_relative("../Drink.rb")
 require_relative("../Customer.rb")
+require_relative("../Pub.rb")
 
 class TestCustomer < MiniTest::Test
 
@@ -9,6 +10,14 @@ class TestCustomer < MiniTest::Test
 
     @customer1 = Customer.new("Kevin", 10)
     @customer2 = Customer.new("Nathan", 20)
+
+    @drink1 = Drink.new("Beer", 4)
+    @drink2 = Drink.new("Cider", 3)
+    @drink3 = Drink.new("Wine", 5)
+
+    @drinks = [@drink1, @drink2, @drink3]
+
+    @pub = Pub.new("The Winchester", 1000, @drinks)
 
   end
 
@@ -18,6 +27,17 @@ class TestCustomer < MiniTest::Test
 
   def test_customer_has_wallet()
     assert_equal(20, @customer2.wallet())
+  end
+
+  def test_reduce_wallet()
+    @customer1.reduce_wallet(@drink2)
+    assert_equal(7, @customer1.wallet())
+  end
+
+  def test_customer_buys_drink()
+    @customer1.buys_drink(@drink2, @pub)
+    assert_equal(7, @customer1.wallet())
+    assert_equal(1003, @pub.till())
   end
 
 end
