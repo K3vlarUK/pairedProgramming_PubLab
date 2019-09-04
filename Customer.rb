603381ed56d4535f66f668cc02bfad739c6b96be
Kevin McDermott <kmcdermott31@gmail.com>
2019-09-04 13:36:42 +0100

    Extensions Complete

--- Customer.rb
+++ Customer.rb
@@ -1,6 +1,6 @@
 class Customer
 
-  attr_reader :name, :wallet
+  attr_reader :name, :wallet, :age, :drunkenness
 
   def initialize(name, wallet, age)
     @name = name
@@ -13,17 +13,31 @@ class Customer
     @wallet -= drink.get_price()
   end
 
+  def check_age()
+    if @age >= 18
+      return true
+    elsif @age < 18
+      return false
+    end
+  end
+
+  def increase_drunkenness(drink)
+    @drunkenness += drink.get_alcohol_level()
+  end
+
   def buys_drink(drink,pub)
-    reduce_wallet(drink)
-    pub.increase_till(drink)
-  end
-
-  def get_age()
-    return @age
-  end
-
-  def get_drunkenness()
-    return @drunkenness
+    if check_age() == true
+      # if (@drunkenness + drink.get_alcohol_level()) < 10
+      if @drunkenness < 10
+        reduce_wallet(drink)
+        pub.increase_till(drink)
+        increase_drunkenness(drink)
+      else
+        return "Too drunk! Get out!"
+      end
+    else
+      return "Too young! Get out!"
+    end
   end
 
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -10,6 +10,7 @@ class TestCustomer < MiniTest::Test
 
     @customer1 = Customer.new("Kevin", 10, 29)
     @customer2 = Customer.new("Nathan", 20, 27)
+    @customer3 = Customer.new("Niall", 50, 16)
 
     @drink1 = Drink.new("Beer", 4, 3)
     @drink2 = Drink.new("Cider", 3, 2)
@@ -34,18 +35,54 @@ class TestCustomer < MiniTest::Test
     assert_equal(7, @customer1.wallet())
   end
 
-  def test_customer_buys_drink()
+  def test_customer_buys_drink__legal()
     @customer1.buys_drink(@drink2, @pub)
     assert_equal(7, @customer1.wallet())
     assert_equal(1003, @pub.till())
+    assert_equal(2, @customer1.drunkenness())
+  end
+
+  def test_customer_buys_drink__illegal()
+    assert_equal("Too young! Get out!", @customer3.buys_drink(@drink2, @pub))
+    assert_equal(50, @customer3.wallet())
+    assert_equal(1000, @pub.till())
+    assert_equal(0, @customer3.drunkenness())
   end
 
   def test_get_age()
-    assert_equal(29, @customer1.get_age())
+    assert_equal(29, @customer1.age())
   end
 
   def test_get_drunkenness()
-    assert_equal(0, @customer2.get_drunkenness())
+    assert_equal(0, @customer2.drunkenness())
+  end
+
+  def test_increase_drunkenness()
+    @customer1.increase_drunkenness(@drink1)
+    assert_equal(3, @customer1.drunkenness())
+  end
+
+  def test_check_age__true()
+    assert_equal(true, @customer1.check_age())
+  end
+
+  def test_check_age__false()
+    assert_equal(false, @customer3.check_age())
+  end
+
+  def test_customer_is_drunk()
+    @customer1.buys_drink(@drink3, @pub)
+    @customer1.buys_drink(@drink3, @pub)
+    @customer1.buys_drink(@drink3, @pub)
+    result = @customer1.buys_drink(@drink3, @pub)
+    assert_equal("Too drunk! Get out!", result)
+  end
+
+  def test_customer_is_not_drunk()
+    @customer1.buys_drink(@drink3, @pub)
+    @customer1.buys_drink(@drink3, @pub)
+    result = @customer1.buys_drink(@drink3, @pub)
+    assert_equal(12, result)
   end
 
 end
